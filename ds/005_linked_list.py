b6e3d7a1968a7096bd8845cae68128641d924a6a
mhassantrq <mhassantrq@gmail.com>
2026-05-27 20:04:29 +0500

    updated linked list

--- ds/005_linked_list.py
+++ ds/005_linked_list.py
@@ -8,7 +8,16 @@ class node():
         self.data = data
         self.next = None
     
-class LinkedList():
-    pass
+head = node(5)
+node1 = node(6)
+node2 = node(7)
 
 
+head.next = node1
+node1.next = node2
+
+curr = head
+
+while curr:
+    print(curr.data)
+    curr = curr.next
